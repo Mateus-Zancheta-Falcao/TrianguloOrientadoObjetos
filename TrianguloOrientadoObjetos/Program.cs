19c59f5f0dc148b767a279045ea0577f325985c4
Mateus Zancheta Falcao <mateuszancheta2005@gmail.com>
2023-04-05 17:11:39 -0300

    Add project files.

---
+++ TrianguloOrientadoObjetos/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOrientadoObjetos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo = new Triangulo();
+
+            Console.WriteLine("Informe o valor de X");
+            triangulo.X = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de Y");
+            triangulo.Y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe o valor de Z");
+            triangulo.Z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{triangulo.VerificarTipo()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TrianguloOrientadoObjetos/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrianguloOrientadoObjetos
+{
+    internal class Triangulo
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public string resposta { get; set; }
+
+        public string VerificarTipo() 
+        {
+            if (X == 0 || Y == 0 || Z == 0)
+            {
+                resposta = "Não pode conter valores do tipo zero!";
+            }
+            else
+            {
+                if (X < Y + Z || Y < X + Z || Z < Y + X)
+                {
+                    Console.WriteLine("\nForma um triângulo\n");
+                }
+                if (X == Y && Y == Z && X == Z)
+                {
+                    resposta = "Triângulo Equilátero";
+                }
+                else if (X == Y || Y == Z || X == Z)
+                {
+                    resposta = "Triângulo Isósceles";
+                }
+                else
+                {
+                    resposta = "Triângulo Escaleno";
+                }
+            }
+                return resposta;
+        }
+    }
+}
